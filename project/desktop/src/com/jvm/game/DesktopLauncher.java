622079356d235090665ddfc2cf3bd836b65c8b2c
jjzsilva9 <jjzsilva9@gmail.com>
2024-02-21 22:56:15 +0000

    default project commit
    initial commit of a default libgdx project in JDK 11 with some additional libraries that may be helpful

---
+++ project/desktop/src/com/jvm/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.jvm.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.jvm.game.MyGdxGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("jvm-game");
+		new Lwjgl3Application(new MyGdxGame(), config);
+	}
+}
