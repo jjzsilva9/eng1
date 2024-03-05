95ed94b6c5e1e8b5abae40641263014804d0bd8a
jjzsilva9 <jjzsilva9@gmail.com>
2024-03-05 13:09:43 +0000

    added Ashley (ECS) to the default project

--- project/core/src/com/jvm/game/JVMGame.java
+++ project/core/src/com/jvm/game/JVMGame.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class JVMGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
