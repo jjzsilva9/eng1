15c552d76303cc9639e63f98c94e241680aec410
Freddie-Higham <fh1061@york.ac.uk>
2024-03-12 15:15:08 +0000

    Collision properties added to tile map

--- project/assets/map/tileset_1.tsx
+++ project/assets/map/tileset_1.tsx
@@ -1,4 +1,34 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="tileset_1" tilewidth="32" tileheight="32" tilecount="1104" columns="48">
  <image source="Slates [32x32px orthogonal tileset by Ivan Voirol].png" width="1536" height="736"/>
+ <tile id="920">
+  <properties>
+   <property name="Collider" value="True"/>
+  </properties>
+ </tile>
+ <tile id="921">
+  <properties>
+   <property name="Collider" value="True"/>
+  </properties>
+ </tile>
+ <tile id="968">
+  <properties>
+   <property name="Collider" value="True"/>
+  </properties>
+ </tile>
+ <tile id="969">
+  <properties>
+   <property name="Collider" value="True"/>
+  </properties>
+ </tile>
+ <tile id="1016">
+  <properties>
+   <property name="Collider" value="True"/>
+  </properties>
+ </tile>
+ <tile id="1017">
+  <properties>
+   <property name="Collider" value="True"/>
+  </properties>
+ </tile>
 </tileset>
