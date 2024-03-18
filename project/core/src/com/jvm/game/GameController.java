eaa66c9ef5e50081c431152fcfd69aa01d5d2a26
jjzsilva9 <jjzsilva9@gmail.com>
2024-03-18 16:48:17 +0000

    finished javadocs for all files

--- project/core/src/com/jvm/game/GameController.java
+++ project/core/src/com/jvm/game/GameController.java
@@ -4,18 +4,26 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-//Handling for multiple screens
+/**
+ * Acts as a screen handler for multiple screens
+ */
 public class GameController extends Game {
 
 
     public static int GAME_WIDTH = 960 ;
     public static int GAME_HEIGHT = 540;
 
+    /**
+     * Start, sets the menu screen
+     */
     public void create() {
 
         this.setScreen(new MenuScreen(this));
     }
 
+    /**
+     * Renders the active screen
+     */
     public void render() {
         super.render();
     }
